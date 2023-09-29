4ffbd7a73d64520a9735282f6a53e3005a2a5884
alarcon steven <devalarcon.23@gmail.com>
2023-09-29 14:52:10 -0500

    Se inicia los estilos de la vista gestion.php

--- views/note/gestion.php
+++ views/note/gestion.php
@@ -3,7 +3,7 @@
         <section class="list">
             <?php $categorias = Helpers::showAllCategories()?>
                 <?php while($cat = $categorias->fetch_object()):?>
-                    <a href="<?=base_url?>Note/getNoteByCAtegoryId?id<?=$cat->id?>"><?=$cat->nombre?></a>
+                    <a  class="bycategory" href="<?=base_url?>Note/getNoteByCAtegoryId?id<?=$cat->id?>"><?=$cat->nombre?></a>
                 <?php endwhile; ?>
         </section>
 </div>
