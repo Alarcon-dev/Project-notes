93ee4ca3eb98b927f40d9d7dc84799479accc90d
alarcon steven <devalarcon.23@gmail.com>
2023-09-29 15:05:55 -0500

    Se termina de dar los estolos del archivo gestion.php

--- views/note/gestion.php
+++ views/note/gestion.php
@@ -2,8 +2,12 @@
     <h2>ver notas por categorias</h2>
         <section class="list">
             <?php $categorias = Helpers::showAllCategories()?>
+            <?php if($categorias->num_rows >= 1):?>
                 <?php while($cat = $categorias->fetch_object()):?>
                     <a  class="bycategory" href="<?=base_url?>Note/getNoteByCAtegoryId?id<?=$cat->id?>"><?=$cat->nombre?></a>
                 <?php endwhile; ?>
+                <?php else: ?>
+                <h2>NO hay categorias Categorias en la lista</h2>
+            <?php endif ?>
         </section>
 </div>
