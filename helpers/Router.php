475c77b7d1c8975bb4e292654148489e7fb1c3e3
alarcon steven <devalarcon.23@gmail.com>
2024-05-13 21:21:26 -0500

    cambios en el enrutador

--- helpers/Router.php
+++ helpers/Router.php
@@ -1,56 +1,55 @@
-<?php 
-class Router{
+<?php
+class Router
+{
 
-    private $controller = controller_default; 
+    private $controller = controller_default;
     private $method = action_default;
-    
-    public function __construct(){
 
-        $url = $this->catchUrl(); 
+    public function __construct()
+    {
 
-        $this->controller = !empty($url[0]) ? $url[0] : controller_default; 
-        
-        if(!file_exists(__DIR__."/../controllers/".$this->controller."Controller.php")){
-            require_once "./controllers/ErrorController.php"; 
-            $error = new ErrorController; 
-            $error->index(); 
-            die(); 
+        $url = $this->catchUrl();
+
+        $this->controller = !empty($url[0]) ? $url[0] : controller_default;
+
+        if (!file_exists(__DIR__ . "/../controllers/" . $this->controller . "Controller.php")) {
+            require_once "./controllers/ErrorController.php";
+            $error = new ErrorController;
+            $error->index();
+            die();
         }
 
-       
-        $this->controller = $this->controller."Controller";
-        require_once "./controllers/".$this->controller.".php"; 
 
-        $this->controller = new $this->controller; 
+        $this->controller = $this->controller . "Controller";
+        require_once "./controllers/" . $this->controller . ".php";
 
-        $this->method = !empty($url[1]) ? $url[1] : action_default; 
+        $this->controller = new $this->controller;
 
-        if(!method_exists($url[0]."Controller", $this->method)){
-            if($this->method !== action_default){
-                require_once "./controllers/ErrorController.php"; 
-                $error = new ErrorController; 
-                $error->index(); 
-                die(); 
+        $this->method = !empty($url[1]) ? $url[1] : action_default;
+
+        if (!method_exists($url[0] . "Controller", $this->method)) {
+            if ($this->method !== action_default) {
+                require_once "./controllers/ErrorController.php";
+                $error = new ErrorController;
+                $error->index();
+                die();
             }
         }
 
-        ob_start(); 
-            $action = $this->method; 
-            $this->controller->$action(); 
-        $print = ob_get_clean(); 
-        require_once "./views/layouts/header.php"; 
-            echo $print; 
-        require_once "./views/layouts/footer.php"; 
-
+        ob_start();
+        $action = $this->method;
+        $this->controller->$action();
+        $print = ob_get_clean();
+        require_once "./views/layouts/header.php";
+        echo $print;
+        require_once "./views/layouts/footer.php";
     }
 
-    public  function catchUrl(){
-        
-        $url = isset($_GET['url']) ? $_GET['url'] : ""; 
+    public  function catchUrl()
+    {
 
-        return explode("/",$url); 
+        $url = isset($_GET['url']) ? $_GET['url'] : "";
+        $url = filter_var($url, FILTER_SANITIZE_URL);
+        return explode("/", $url);
     }
-
-
-   
 }
